7d0e5ed468c5942b2250d9350f09d41ca8b5926a
John Doe <hardik.a.marlapudi@gmail.com>
2025-02-16 01:03:32 -0500

    Need to fix the JavaScript file

--- apps/index.js
+++ apps/index.js
@@ -1,7 +1,11 @@
 
-document.getElementById("app-button").onclick = function() {
+/* 
+    // FIX ME: The if layer isn't working correctly.
+
+    document.getElementById("app-button").onclick = function() {
     
     if (document.getElementById("app-button").onclick = function() {
         console.log("app button clicked");
     });
 }
+*/

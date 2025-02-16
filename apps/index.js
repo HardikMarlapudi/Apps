d03b3e2f2b58ded28a39e24040858c1139a1376f
John Doe <hardik.a.marlapudi@gmail.com>
2025-02-16 01:00:51 -0500

    Need to fix the Javascript file

---
+++ apps/index.js
@@ -0,0 +1,7 @@
+
+document.getElementById("app-button").onclick = function() {
+    
+    if (document.getElementById("app-button").onclick = function() {
+        console.log("app button clicked");
+    });
+}
